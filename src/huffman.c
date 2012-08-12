58682671a2c9de69ae6d98cbabfff6571f51522a
hamidrez <hamid@dadkhah.ca>
2012-08-12 14:25:31 -0700

    finished and fixed the main

--- src/huffman.c
+++ src/huffman.c
@@ -9,14 +9,18 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
+#include <unistd.h>
 
 
-char *compress(char *infile, char *outfile)
+char *ALPHA[26] = { "1010", "011110", "10111", "1110", "010", "011111", "11010",
+				    "00010", "0110", "111110000", "111111", "10110","11011", "0000", 
+				    "11110", "0111001", "11111000101", "1001", "1000", "1100", "00011",
+				    "1111101", "011101", "11111000100", "1111100011", "0111000" };
+char ALPHA_REVERSE[26] = { 'a' , 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
+   						   'n', 'o', 'p' , 'q', 'r' , 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+void compress(char *infile, char *outfile)
 {
-    char *ALPHA[26] = { "1010", "011110", "10111", "1110", "010", "011111", "11010",
-    				    "00010", "0110", "111110000", "111111", "10110","11011", "0000", 
-    				    "11110", "0111001", "11111000101", "1001", "1000", "1100", "00011",
-    				    "1111101", "011101", "11111000100", "1111100011", "0111000" };
 	char symbol_to_encode;
 	FILE *in = fopen(infile, "r");
 	FILE *out = fopen (outfile , "w");
@@ -26,7 +30,7 @@ char *compress(char *infile, char *outfile)
     if (in == NULL)
     {
     	printf("Cannot open %s\n", infile);
-        return NULL;
+    	abort();
     }
 		
 	//set the pointer to the begining of file	
@@ -61,36 +65,18 @@ char *compress(char *infile, char *outfile)
 			 //Now write the string buffer to the file
 			 fwrite (buffer,1,strlen(buffer),out);
 		}
-		
 	}
+	printf("Compression is done!\n");
     fclose(in);
     fclose (out);
     
 }
 
-
-char* substring(const char* str, size_t begin, size_t len) 
-{ 
-  if (str == 0 || strlen(str) == 0 || strlen(str) < begin || strlen(str) < (begin+len)) 
-    return 0; 
-
-  return strndup(str + begin, len); 
-} 
-
-
-char *decompress (char *infile, char *outfile)
+void decompress (char *infile, char *outfile)
 {
-    char *ALPHA[26] = { "1010", "011110", "10111", "1110", "010", "011111", "11010",
-    				    "00010", "0110", "111110000", "111111", "10110","11011", "0000", 
-    				    "11110", "0111001", "11111000101", "1001", "1000", "1100", "00011",
-    				    "1111101", "011101", "11111000100", "1111100011", "0111000" };
-   	char *ALPHA_REVERSE[26] = { "a" , "b", "c", "d", "e", "f", "g",
-    				    "h", "i", "j", "k","l", "m","n" 
-    				    "o", "p", "q", "r", "s", "t", "u"
-    				    "v", "w", "x", "y", "z" };
 	char symbol_to_encode;
 	FILE *in = fopen(infile, "r");
-	//FILE *out = fopen (outfile , "w");
+	FILE *out = fopen (outfile , "w");
 	int currentbuffersize;
     int ch;
     int i;
@@ -99,11 +85,11 @@ char *decompress (char *infile, char *outfile)
     if (in == NULL)
     {
     	printf("Cannot open %s\n", infile);
-        return NULL;
+        abort();
     }
 		
 	//set the pointer to the begining of file	
-	//fseek ( out , 0 , SEEK_SET );
+	fseek ( out , 0 , SEEK_SET );
 	
 	i = 1;
 	j = 0;
@@ -115,12 +101,12 @@ char *decompress (char *infile, char *outfile)
        	{
    			if (strncmp(buffer, "001", 3) == 0)
    			{
-   				printf("%s", " ");
+   				fputc(32, out);
    				i = 1;
    			}
    			if (strncmp(buffer, ALPHA[4], 3) == 0)
    			{
-   				printf("%s", "e");
+   				fputc(ALPHA_REVERSE[4], out);
    				i = 1;
    			}
    			
@@ -129,37 +115,37 @@ char *decompress (char *infile, char *outfile)
        	{
    			if (strncmp(buffer, ALPHA[13], 4) == 0)
    			{
-   				printf("%s", "n");
+   				fputc(ALPHA_REVERSE[13], out);
    				i = 1;
    			}
    			if (strncmp(buffer, ALPHA[8], 4) == 0)
    			{
-   				printf("%s", "i");
+   				fputc(ALPHA_REVERSE[8], out);
    				i = 1;
    			}
    			if (strncmp(buffer, ALPHA[18], 4) == 0)
    			{
-   				printf("%s", "s");
+   				fputc(ALPHA_REVERSE[18], out);
    				i = 1;
    			}
    			if (strncmp(buffer, ALPHA[17], 4) == 0)
    			{
-   				printf("%s", "r");
+   				fputc(ALPHA_REVERSE[17], out);
    				i = 1;
    			}
    			if (strncmp(buffer, ALPHA[0], 4) == 0)
    			{
-   				printf("%s", "a");
+   				fputc(ALPHA_REVERSE[0], out);
    				i = 1;
    			}
    			if (strncmp(buffer, ALPHA[19], 4) == 0)
    			{
-   				printf("%s", "t");
+   				fputc(ALPHA_REVERSE[19], out);
    				i = 1;
    			}
    			if (strncmp(buffer, ALPHA[3], 4) == 0)
    			{
-   				printf("%s", "d");
+   				fputc(ALPHA_REVERSE[3], out);
    				i = 1;
    			}
        	}
@@ -167,37 +153,37 @@ char *decompress (char *infile, char *outfile)
        	{
    			if (strncmp(buffer, ALPHA[7], 5) == 0)
    			{
-   				printf("%s", "h");
+   				fputc(ALPHA_REVERSE[7], out);
    				i = 1;
    			}
    			if (strncmp(buffer, ALPHA[20], 5) == 0)
    			{
-   				printf("%s", "u");
+   				fputc(ALPHA_REVERSE[20], out);
    				i = 1;
    			}
    			if (strncmp(buffer, ALPHA[11], 5) == 0)
    			{
-   				printf("%s", "l");
+   				fputc(ALPHA_REVERSE[11], out);
    				i = 1;
    			}
    			if (strncmp(buffer, ALPHA[2], 5) == 0)
    			{
-   				printf("%s", "c");
+   				fputc(ALPHA_REVERSE[2], out);
    				i = 1;
    			}
    			if (strncmp(buffer, ALPHA[6], 5) == 0)
    			{
-   				printf("%s", "g");
+   				fputc(ALPHA_REVERSE[6], out);
    				i = 1;
    			}
    			if (strncmp(buffer, ALPHA[12], 5) == 0)
    			{
-   				printf("%s", "m");
+   				fputc(ALPHA_REVERSE[12], out);
    				i = 1;
    			}
    			if (strncmp(buffer, ALPHA[14], 5) == 0)
    			{
-   				printf("%s", "o");
+   				fputc(ALPHA_REVERSE[14], out);
    				i = 1;
    			}
        	}
@@ -205,22 +191,22 @@ char *decompress (char *infile, char *outfile)
        	{
    			if (strncmp(buffer, ALPHA[22], 6) == 0)
    			{
-   				printf("%s", "w");
+   				fputc(ALPHA_REVERSE[22], out);
    				i = 1;
    			}
    			if (strncmp(buffer, ALPHA[1], 6) == 0)
    			{
-   				printf("%s", "b");
+   				fputc(ALPHA_REVERSE[1], out);
    				i = 1;
    			}
    			if (strncmp(buffer, ALPHA[5], 6) == 0)
    			{
-   				printf("%s", "f");
+   				fputc(ALPHA_REVERSE[5], out);
    				i = 1;
    			}
    			if (strncmp(buffer, ALPHA[10], 6) == 0)
    			{
-   				printf("%s", "k");
+   				fputc(ALPHA_REVERSE[10], out);
    				i = 1;
    			}
        	}
@@ -228,17 +214,17 @@ char *decompress (char *infile, char *outfile)
        	{
    			if (strncmp(buffer, ALPHA[25], 7) == 0)
    			{
-   				printf("%s", "z");
+   				fputc(ALPHA_REVERSE[25], out);
    				i = 1;
    			}
    			if (strncmp(buffer, ALPHA[15], 7) == 0)
    			{
-   				printf("%s", "p");
+   				fputc(ALPHA_REVERSE[15], out);
    				i = 1;
    			}
    			if (strncmp(buffer, ALPHA[21], 7) == 0)
    			{
-   				printf("%s", "v");
+   				fputc(ALPHA_REVERSE[21], out);
    				i = 1;
    			}
        	}
@@ -246,7 +232,7 @@ char *decompress (char *infile, char *outfile)
        	{
        		if (strncmp(buffer, "11111001", 8) == 0)
    			{
-   				printf("%c", 225);
+   				fputc(225, out);
    				i = 1;
    			}
        	}
@@ -254,46 +240,70 @@ char *decompress (char *infile, char *outfile)
        	{
        		if (strncmp(buffer, ALPHA[9] , 9) == 0)
    			{
-   				printf("%s", "j");
+   				fputc(ALPHA_REVERSE[9], out);
    				i = 1;
    			}
        	}
        	if (i == 11)
        	{
-       		if (strncmp(buffer, ALPHA[22] , 10) == 0)
+       		if (strncmp(buffer, ALPHA[24] , 10) == 0)
    			{
-   				printf("%s", "y");
+   				fputc(ALPHA_REVERSE[24], out);
    				i = 1;
    			}
        	}
         else if(i == 12)
         {
-        	if (strncmp(buffer, ALPHA[21] , 11) == 0)
+        	if (strncmp(buffer, ALPHA[23] , 11) == 0)
    			{
-   				printf("%s", "x");
+   				fputc(ALPHA_REVERSE[23], out);
    				i = 1;
    			}
    			if (strncmp(buffer, ALPHA[16] , 11) == 0)
    			{
-   				printf("%s", "q");
+   				fputc(ALPHA_REVERSE[16], out);
    				i = 1;
    			}
-        	//printf("%s", buffer);
         }
         
 		buffer[i-1] = ch;
 		i++;
 
     }
+    printf("Decompression is done!\n");
     fclose(in);
-    //fclose (out);
+    fclose (out);
     
 }
 
 
-int main()
+int main(int argc, char *argv[])
 {
-	//compress("README", "compression.out");
-	decompress("compression.out", "original.out");
-	return 0;
+   int index;
+   int c;
+   opterr = 0;
+   
+   /* Clear The Screen */
+   system ("clear");
+   
+   while ((c = getopt (argc, argv, "cd")) != -1)
+     switch (c)
+       {
+       case 'c':
+       	 compress(argv[2], argv[3]);
+         break;
+       case 'd':
+         decompress(argv[2], argv[3]);
+         break;
+       case '?':
+         if (isprint (optopt))
+           fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+         else
+           fprintf (stderr,"Unknown option character `\\x%x'.\n",optopt);
+         return 1;
+       default:
+         abort();
+       }
+       	
+   return 1;
 }
