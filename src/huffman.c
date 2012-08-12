0a99e63aca070ee9742ca7edb5f682dc7c38ee8a
hamidrez <hamid@dadkhah.ca>
2012-08-11 20:02:11 -0700

    finished compress and save it to a file, remeber this is a pure software implementation that mostly does the concept based on the slides

--- src/huffman.c
+++ src/huffman.c
@@ -2,37 +2,69 @@
 /*                A simple implementation of the Huffman coding             */
 /*                author: hamid@dadkhah.ca									*/
 /*  			  Hamidreza Eftekharadkhah - V00661878                      */
+/*  			  Rhett Dobson - V00214277			                        */
 /*                                                                          */
 /****************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
 
 char *compress(char *fileName)
 {
-    FILE *file = fopen(fileName, "r");
-    char *code;
-    size_t n = 0;
-    int c;
+    char *ALPHA[26] = { "1010", "011110", "10111", "1110", "010", "011111", "11010",
+    				    "00010", "0110", "111110000", "111111", "10110","11011", "0000", 
+    				    "11110", "0111001", "11111000101", "1001", "1000", "1100", "00011",
+    				    "1111101", "011101", "11111000100", "1111100011", "0111000" };
+	char symbol_to_encode;
+	FILE *file = fopen(fileName, "r");
+	FILE *pFile = fopen ("compressed.txt" , "w");
+	char buffer[10];
+    int ch;
 
     if (file == NULL)
-        return NULL; //could not open file
-
-    code = malloc(1000);
-
-    while ((c = fgetc(file)) != EOF)
     {
-        code[n++] = (char) c;
+    	printf("Cannot open %s\n", fileName);
+        return NULL;
     }
-
-    // don't forget to terminate with the null character
-    code[n] = '\0';        
-
-    return code;
+	
+	while (1) 
+	{
+		ch = fgetc(file);
+		ch = tolower(ch);
+		if (ch == EOF)
+			break;
+		else
+		{
+			 if (ch == 32)
+			 {
+			 	//printf( "%s", "001");
+			 }
+			 else if (ch == 225)
+			 {
+			 	//printf( "%s", "11111001");
+			 }
+			 else
+			 {	
+			 	if ((ch > 96) & (ch < 123))	 
+			 		//printf( "%s", ALPHA[(int)ch-97]);
+					strcpy(buffer,ALPHA[(int)ch-97]);
+					fwrite (buffer,1,strlen(buffer),pFile);
+			 }
+			 
+			 //printf( "%c", (char)ch);
+		}
+		
+	}
+    fclose(file);
+    fclose (pFile);
+    
 }
+
 
 int main()
 {
-	printf ("%s\n", compress("README"));
+	compress("README");
 	return 0;
 }
